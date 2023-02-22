09f40f39f3c97cfb6ea6b969ab60e700e63eda54
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 15:41:01 -0500

    Add files via upload

---
+++ Chapter 5 programs #6.py
@@ -0,0 +1,19 @@
+#6
+import random
+def main():
+#random number separately using random.randint
+    num1=random.randint(100,125)
+    print(num1)
+    num2=random.randint(100,125)
+    print(num2)
+    num3=random.randint(100,125)
+    print(num3)
+    num4=random.randint(100,125)
+    print(num4)
+    num5=random.randint(100,125)
+    print(num5)
+
+#sum
+    sum=num1+num2+num3+num4+num5
+    print("Your total sum is", sum)
+main()
