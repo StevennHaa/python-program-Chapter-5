f8566f8a268c63cdb7845eedd99378986f0c0b45
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 15:48:49 -0500

    Add files via upload

---
+++ Chapter 5 programs #7.py
@@ -0,0 +1,8 @@
+#7
+import random
+def main():
+    num1=random.randint(25,50)
+    num2=random.randint(1,25)
+    print(num1, "+", num2, "=", num1+num2)
+    print(num1, "-", num2, "=", num1-num2)
+main()
