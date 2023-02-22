b866deef7b6af343bb9adb2d9a6629652249a72c
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 12:05:20 -0500

    Delete Chapter 5 programs #3.py

--- Chapter 5 programs #3.py
+++
@@ -1,9 +0,0 @@
-#3
-from random import randrange
-def main():
-    minvalue=eval(input("Enter the minimum value"))
-    maxvalue=eval(input("Enter the maximum value"))
-    r=randrange(minvalue,maxvalue)
-    print(r)
-main()
-
