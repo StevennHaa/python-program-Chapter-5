93648feb26f313c896280fdbc0185fadb6b6b8be
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 11:59:21 -0500

    Add files via upload

---
+++ Chapter 5 programs #2.py
@@ -0,0 +1,9 @@
+#3
+from random import randrange
+def main():
+    minvalue=eval(input("Enter the minimum value"))
+    maxvalue=eval(input("Enter the maximum value"))
+    r=randrange(minvalue,maxvalue)
+    print(r)
+main()
+
