fc4ac7101fb9778162e2ddb639a9dee47ece96fa
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 12:05:26 -0500

    Delete Chapter 5 programs #4.py

--- Chapter 5 programs #4.py
+++
@@ -1,9 +0,0 @@
-#4
-from math import*
-def main():
-    y=pi
-    radius=[2,5,6]
-    for x in range (3):
-        area=y*radius[x]**2
-        print("{0:<10s}{1:<5.1f}{2:<5}{3:<10.1f}".format("The area of a circle with the radius of", radius[x],"is", area))
-main()
