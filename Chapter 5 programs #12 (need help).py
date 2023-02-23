50b7e95c76614aa663b9624fa2b67d870f049ecf
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 20:37:31 -0500

    Add files via upload

---
+++ Chapter 5 programs #12 (need help).py
@@ -0,0 +1,10 @@
+#12
+from random import randrange
+def main():
+    for x in range(1,61):
+        r=randrange(1,5001)
+    s_num= min (r)
+    print("The smallest number in the randomizer is", s_num)
+    s_max= max (r)
+    print("The largest number in the randomizer is", s_max)
+main()
