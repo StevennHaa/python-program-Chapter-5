d9a476b1319183283a3dd05f7452d77e196d942a
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-22 21:08:13 -0500

    Add files via upload

---
+++ Chapter 5 programs #14.py
@@ -0,0 +1,10 @@
+#14
+
+from math import*
+def main():
+    a=eval(input("Enter the A value"))
+    b=eval(input("Enter the B value"))
+    c=a**2+b**2
+    y=sqrt(c)
+    print("Using the pythagorean theorem your c value is",y)
+main()
