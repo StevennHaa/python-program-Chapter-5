13caa2abedf7f1ae20dc4b59025745fc0c8aebd2
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-23 12:10:04 -0500

    Add files via upload

---
+++ Chapter 5 Programs #15.py
@@ -0,0 +1,10 @@
+#15
+from math import*
+def main():
+    num=eval(input("Enter a number "))
+    print(num)
+    squared=num**2
+    print(num, "squared is ", squared)
+    y=sqrt(num)
+    print("The squareroot of",num, "is", y)
+main()
