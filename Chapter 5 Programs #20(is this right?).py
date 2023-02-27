90265874e388f6050b7d56ac21e0e159e5dfd0c8
Steven <123708698+StevennHaa@users.noreply.github.com>
2023-02-27 12:09:21 -0500

    Add files via upload

---
+++ Chapter 5 Programs #20(is this right?).py
@@ -0,0 +1,21 @@
+#20
+from random import randrange
+def main():
+    start=4
+    forward=0
+    backwards=0
+    count=0
+    while start>0 and start<7:
+        r=randrange(1,3)
+        if r==1:
+            count=count+1
+            backwards=backwards+1
+            start=start-1
+            print("You took a", backwards, "step backwards")
+        if r==2:
+            count=count+1
+            forward=forward+1
+            start=start+1
+            print("You took a", forward, "step forward")
+    print("It took you", count, "steps to get off the bridge")
+main()
